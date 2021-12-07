e219b9c38d7a37311a26bd1041e0cbc79e9926a9
Jerry Xu <28634832+jjjxu@users.noreply.github.com>
2021-12-06 23:17:40 -0500

    D7 Prep

---
+++ src/day7/day7.java
@@ -0,0 +1,32 @@
+package day7;
+
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+class PairPoint {
+	Point p1;
+	Point p2;
+}
+
+public class day7 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader input = new BufferedReader(new FileReader("day_7_input.txt"));
+		ArrayList<String> inputs = new ArrayList<>();
+		String readLine = input.readLine();
+		StringTokenizer st = new StringTokenizer(readLine);
+		
+		while (readLine != null) {
+			inputs.add(readLine);
+			
+			readLine = input.readLine();
+			
+		}
+		System.out.println(inputs.size());
+		
+	}
+	
+}
