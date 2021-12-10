76185eb4fd57172876efd1165c8e70b53edc7b99
Jerry Xu <28634832+jjjxu@users.noreply.github.com>
2021-12-10 00:22:47 -0500

    D10
    gah
    errors galore

--- src/day10/day10b.java
+++ src/day10/day10b.java
@@ -1,12 +1,11 @@
 package day10;
 
-import java.awt.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
 
-public class day10 {
+public class day10b {
 	static boolean[][] is_low = new boolean[100][100];
 	static int[][] floodfill = new int[100][100];
 	static HashMap<Integer, Integer> map = new HashMap<>();
@@ -21,6 +20,8 @@ public class day10 {
 		StringTokenizer st = new StringTokenizer(readLine);
 		
 		int count = 0;
+		int count2 = 0;
+		ArrayList<Long> counts = new ArrayList<>();
 		outer: while (readLine != null) {
 			String rest = readLine.trim();
 			char[] chars = rest.toCharArray();
@@ -71,11 +72,34 @@ public class day10 {
 					}
 				}
 			}
+			
+			if (!mySyntax.isEmpty()) {
+				long tempCount = 0;
+				for (int i = mySyntax.size() - 1; i >= 0; i--) {
+					tempCount *= 5;
+					if (mySyntax.get(i) == '(') {
+						tempCount += 1;
+					} else if (mySyntax.get(i) == '[') {
+						tempCount += 2;
+					} else if (mySyntax.get(i) == '{') {
+						tempCount += 3;
+					} else if  (mySyntax.get(i) == '<') {
+						tempCount += 4;
+					}
+				}
+				counts.add(tempCount);
+				count2 += tempCount;
+			}
 
 			readLine = input.readLine();
 		}
 		
+		Collections.sort(counts);
+		
 		System.out.println(count);
+		System.out.println(count2);
+		
+		System.out.println(counts.get(counts.size() / 2));
 		
 	}
 	
