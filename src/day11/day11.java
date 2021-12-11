5962017a02f0a43608deefd26030678b43c23b73
Jerry Xu <28634832+jjjxu@users.noreply.github.com>
2021-12-11 00:28:02 -0500

    D11
    should've done smart method but didn't kno smart method

--- src/day11/day11.java
+++ src/day11/day11.java
@@ -1,37 +1,101 @@
 package day11;
 
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class day11 {
-	static boolean[][] is_low = new boolean[100][100];
-	static int[][] floodfill = new int[100][100];
-	static HashMap<Integer, Integer> map = new HashMap<>();
-	static int ymax = 0;
-	static int x = 0;
-	static int[][] grid = new int[100][100];
 	
 	public static void main(String[] args) throws IOException {
-		BufferedReader input = new BufferedReader(new FileReader("day_10_input.txt"));
+		BufferedReader input = new BufferedReader(new FileReader("day_11_input.txt"));
 		
 		String readLine = input.readLine();
 		StringTokenizer st = new StringTokenizer(readLine);
+		int[][] grid = new int[10][10];
+		
 		
 		int count = 0;
 		outer:while (readLine != null) {
 			String rest = readLine.trim();
-			
-			int y = 0;
+			char[] chars = rest.toCharArray();
 			st = new StringTokenizer(rest, "");
+			int y = 0;
+			for (int i = 0; i < chars.length; i++) {
+				grid[count][i] = Integer.parseInt(chars[i] + "");
+			}
+			count++;
 			
 			readLine = input.readLine();
 		}
+
+		int flash_count = 0;
+		int step_count = 0;
+		stupid: while(true) {
+			HashSet<Point> flashed = new HashSet<Point>();
+			HashSet<Point> has_flashed = new HashSet<Point>();
+			int can_flash = 0;
+			for (int j = 0; j < 10; j++) {
+				for (int k = 0; k < 10; k++) {
+					grid[j][k]++;
+					if (grid[j][k] > 9) {
+						flash_count++;
+						flashed.add(new Point(j, k));
+						grid[j][k] = 0;
+						has_flashed.add(new Point(j, k));
+					}
+				}
+			}
+
+			step_count++;
+			while(flashed.size() > 0) {
+
+				for (int j = 0; j < 10; j++) {
+					for (int k = 0; k < 10; k++) {
+						if (flashed.contains(new Point(j, k))) {
+							flashed.remove(new Point(j, k));
+							HashSet<Point> points = new HashSet<Point>();
+							points.add(new Point(j - 1, k - 1));
+							points.add(new Point(j - 1, k));
+							points.add(new Point(j - 1, k + 1));
+							points.add(new Point(j, k - 1));
+							points.add(new Point(j, k + 1));
+							points.add(new Point(j + 1, k - 1));
+							points.add(new Point(j + 1, k));
+							points.add(new Point(j + 1, k + 1));
+							for (Point p : points) {
+								if (p.x >= 0 && p.x < 10 && p.y >= 0 && p.y < 10) {
+									if (has_flashed.contains(p)) {
+										continue;
+									}
+									grid[p.x][p.y]++;
+									if (grid[p.x][p.y] > 9) {
+										flashed.add(p);
+										has_flashed.add(p);
+										grid[p.x][p.y] = 0;
+										flash_count++;
+									}
+								}
+							}
+						}
+						
+					}
+				}
+			}
+			if (has_flashed.size() == 100) {
+				System.out.println(step_count);
+				break stupid;
+			}
+//			System.out.println(grid);
+			
+		}
 		
-		System.out.println(count);
+		
+//		System.out.println(flash_count);
 		
 	}
 	
