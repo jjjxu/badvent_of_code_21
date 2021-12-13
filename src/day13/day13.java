f4f7259ae1d7ded4f2312eaa09be450c9d62a73c
Jerry Xu <28634832+jjjxu@users.noreply.github.com>
2021-12-12 23:28:00 -0500

    D13 Prep

---
+++ src/day13/day13.java
@@ -0,0 +1,38 @@
+package day13;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class day13 {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader input = new BufferedReader(new FileReader("day_13_input.txt"));
+		
+		String readLine = input.readLine();
+		StringTokenizer st = new StringTokenizer(readLine);
+		ArrayList<String> raw_lines = new ArrayList<String>();
+		ArrayList<char[]> raw_chars = new ArrayList<>();
+		
+		int x = 0;
+		outer:while (readLine != null) {
+			String rest = readLine.trim();
+			char[] chars = rest.toCharArray();
+			st = new StringTokenizer(rest, "");
+			int y = 0;
+			raw_lines.add(rest);
+			raw_chars.add(chars);
+			
+			x++;
+			readLine = input.readLine();
+			
+		}
+		
+		System.out.println(raw_lines.size());
+		
+	}
+	
+	
+}
