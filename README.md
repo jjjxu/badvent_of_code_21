943fe181bf96169a9bc47d8ed868ae82650492db
Jerry Xu <28634832+jjjxu@users.noreply.github.com>
2021-12-20 01:06:11 -0500

    D20

--- README.md
+++ README.md
@@ -17,13 +17,13 @@ My Python code makes use of `numpy` very heavily.
 
 | Day | P1 solve time | P1 rank |  P2 solve time  | P2 rank |
 |:---:|:-------------:|:-------:|:---------------:|:-------:|
-|  1  | started late  |    -    |   started late  |    -    |
+|  1  | started late  |    -    |  started late   |    -    |
 |  2  |     05:20     |  3450   |  07:26 (02:06)  |  2460   |
 |  3  |     06:50     |  1288   |  27:44 (20:54)  |  2077   |
 |  4  |     17:04     |   865   |  18:56 (01:52)  |   520   |
 |  5  |     10:07     |   670   |  22:47 (12:40)  |  1241   |
 |  6  |     4:23      |   407   |  10:39 (06:16)  |   546   |
-|  7  |     6:43      |  2585   |   7:46 (01:03)  |  1026   |
+|  7  |     6:43      |  2585   |  7:46 (01:03)   |  1026   |
 |  8  |     7:40      |   982   | 1:02:09 (54:29) |  2205   |
 |  9  |     20:03     |  4713   |  32:49 (12:46)  |  2181   |
 | 10  |     4:56      |   223   |  10:28 (05:32)  |   273   |
@@ -35,8 +35,11 @@ My Python code makes use of `numpy` very heavily.
 | 16  |     30:17     |   335   |  38:02 (07:45)  |   364   |
 | 17  |     14:02     |   435   |  18:20 (04:18)  |   348   |
 | 18  |    1:43:58    |  1280   | 1:51:36 (07:38) |  1251   |
+| 19  | too difficult |    -    |  too difficult  |    -    |
+| 20  |     52:02     |  1774   |  59:48 (07:46)  |  1812   |
 
 Notes: 
 
 - Day 1 started late
 - Day 7 had a submission issue because servers were overloaded
+- Day 19 was too difficult, did not solve within 24hrs. 
--- src/day20/day20.py
+++ src/day20/day20.py
@@ -4,14 +4,78 @@ from scipy import stats
 
 sentinel = -1000000
 
+enhance_string = ''
 
-input_arr_str = []
-input_arr_chars = []
+first_line = True
+
+data = []
 
 with open('day_20_input.txt') as file:
     for line in file:
         line = line.strip()
-        input_arr_str.append(line)
-        input_arr_chars.append(list(line))
+        if len(line) == 0:
+            continue
 
-print(len(input_arr_str))
+        new_str = line.replace(".", "0").replace("#", "1")
+        if first_line:
+            enhance_string = new_str
+            first_line = False
+            continue
+
+        row = []
+        for char in new_str:
+            row.append(int(char))
+
+        data.append(row)
+
+data = np.array(data)
+
+print("read done")
+
+
+def pad_data(inp_data, dist=40):
+    data_dim = inp_data.shape
+
+    empty_buffer_row = np.zeros((dist, data_dim[0]))
+    inp_data = np.concatenate((empty_buffer_row, inp_data, empty_buffer_row), axis=0)
+
+    data_dim = inp_data.shape
+
+    empty_buffer_col = np.zeros((data_dim[0], dist))
+    inp_data = np.concatenate((empty_buffer_col, inp_data, empty_buffer_col), axis=1)
+
+    inp_data = inp_data.astype(int)
+    return inp_data
+
+
+data = pad_data(data, 10)
+
+print("pad done")
+
+
+def run_img(data):
+    data_dim = data.shape
+    new_data = np.zeros(data_dim)
+    new_data = new_data.astype(int)
+    for x in range(data_dim[0]):
+        for y in range(data_dim[1]):
+            if x == 0 or x == data_dim[0] - 1 or y == 0 or y == data_dim[1] - 1:
+                new_data[x][y] = 1 - data[1][1]
+                continue
+            local_data = data[x - 1:x + 2, y - 1:y + 2]
+            local_data = local_data.flatten()
+            int_data = local_data.dot(2 ** np.flip(np.arange(9)).astype(int))
+            new_data[x, y] = enhance_string[int_data]
+    for x in range(data_dim[0]):
+        for y in range(data_dim[1]):
+            if not(x == 0 or x == data_dim[0] - 1 or y == 0 or y == data_dim[1] - 1):
+                continue
+            new_data[x][y] = new_data[1][1]
+    return new_data
+
+for i in range(25):
+    data = run_img(data)
+    data = run_img(data)
+    data = pad_data(data, 10)
+    print(str(i) + " " + str(np.sum(data)))
+
