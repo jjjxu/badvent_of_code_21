92d0dccd1d46d1773368f1f9eda291a076f4ebb0
Jerry Xu <28634832+jjjxu@users.noreply.github.com>
2021-12-20 23:54:03 -0500

    D21 Prep

---
+++ src/day21/day21.py
@@ -0,0 +1,17 @@
+import re
+import numpy as np
+from scipy import stats
+
+sentinel = -1000000
+
+
+input_arr_str = []
+input_arr_chars = []
+
+with open('day_21_input.txt') as file:
+    for line in file:
+        line = line.strip()
+        input_arr_str.append(line)
+        input_arr_chars.append(list(line))
+
+print(len(input_arr_str))
